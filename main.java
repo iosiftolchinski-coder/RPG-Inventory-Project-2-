45c92bbdae4a9d7be93935e7072b699e91d78ab8
iosiftolchinski-coder <iosif.tolchinski@ht-la.org>
2026-03-25 22:40:38 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Bag bag = new Bag(5, 20);
+
+        bag.open();
+        bag.carry();
+
+        Weapon hammer = new Weapon("hammer", "big axe", 5, 15);
+        Food olivie = new food("olivie", "regenerates", 1, 20);
+        Gun pistol = new Gun("pistol", "ammo", 1, 3);
+        Key key = new Key("goodkey", "opens", 1);
+
+        bag.store(hammer);
+        bag.store(olivie);
+        bag.store(pistol);
+        bag.store(goodkey);
+
+        bag.show();
+
+        sword.use();
+        potion.use();
+        arrow.use();
+        key.use();
+
+        bag.search("olivie");
+        bag.remove(olivie);
+        bag.show();
+
+        bag.empty();
+        bag.show();
+    }
+}
