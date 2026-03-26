205e098a41fabf679a7a84bb0c8099679042fa9e
iosiftolchinski-coder <iosif.tolchinski@ht-la.org>
2026-03-25 22:34:30 -0700

    Create typesofbags.java

---
+++ typesofbags.java
@@ -0,0 +1,23 @@
+public class Bag extends Bag {
+    public Bag() {
+        super(10);
+    }
+}
+
+public class Purse extends Bag {
+    public Purse() {
+        super(5);
+    }
+}
+
+public class Luggage extends Bag {
+    public Luggage() {
+        super(8);
+    }
+}
+
+public class Baggage extends Bag {
+    public Baggage() {
+        super(15);
+    }
+}
